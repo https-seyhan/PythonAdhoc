f4457e96da7e45a05d03d221f334153c2e2c35c0
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-07-31 19:38:07 +1000

    Add files via upload

--- costAnalysis.py
+++ costAnalysis.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 import  seaborn as sb
 import os
-
+sb.set(style="whitegrid")
 os.chdir('/home/saul/Desktop')
 
 pd.options.display.float_format = '{:.1f}'.format
-#figsize = (1500 / 50, 400 / 50)
-#fig, ax = plt.subplots(1, 1, figsize=figsize)
+figsize = (1500 / 50, 400 / 50)
+fig, ax = plt.subplots(1, 1, figsize=figsize)
 
 cost = pd.read_csv('spend.csv', sep=',')
 
@@ -22,10 +22,18 @@ print(cost.info())
 cost.set_index('date')
 cost.sort_values(by='date', inplace=True)
 print(cost['date'])
-print('Total Market Cost is  %0.1f in % 2d days' % (cost['cost'].sum(), (cost['date'].max() - cost['date'].min()).days))
+print('Total Market Cost is  %0.1f in % 2d days with Daily spend is %0.1f' % (cost['cost'].sum(), (cost['date'].max() - cost['date'].min()).days,
+                                                                              cost['cost'].sum() / (cost['date'].max() - cost['date'].min()).days))
 #cost.plot(cost['cost'])
 #sb.lineplot(x="date", y="cost", data=cost)
+#sb.distplot(cost)
 #ax.plot(cost['cost'])
-#plt.show()
+ax = sb.barplot(x="type", y="cost", data=cost, estimator=sum)
+#g = sb.catplot(x="type", y="cost", hue="market", data=cost,height=6, kind="bar", palette="muted")
+#g.despine(left=True)
+#g.set_ylabels("Cost")
+#g.set_xlabels(("Cost Type"))
+
+plt.show()
 #cost.sort(cost.date)
 #cost['newdate']
