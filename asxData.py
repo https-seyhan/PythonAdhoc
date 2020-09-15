236632a2c81703e4d99e315d684c9389a57eb8ff
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-09-15 23:04:18 +1000

    Add files via upload

---
+++ asxData.py
@@ -0,0 +1,18 @@
+import scrapy
+
+#run as scrapy crawl asxdataextract -o output.csv in the terminal
+
+class AsxDataExtract(scrapy.Spider):
+    name='asxdataextract'
+    allowed_domains = ["listcorp.com/asx/sectors",]
+    start_urls = ["https://www.listcorp.com/asx/sectors/information-technology"]
+    #category = 'information-technology'
+    #links = {}
+    print("Test")
+
+    def parse(self, response):
+
+        query = '//*[@class="v-datatable v-table theme--light"]//tbody/tr'
+        #query= '//*[@class ="v-table__overflow"]//tbody/tr'
+        urls = response.xpath(query).extract()
+        print("URLS!!!!!  ",urls)
