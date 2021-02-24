28899aefbf327dbd12c225a1399e993b4775fa94
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-25 10:14:52 +1100

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,17 @@
+from Subject import *
+
+class Student(object):
+    def __init__(self):
+        self._subjects = {}
+        
+    def subject(self, name):
+        if name not in self._subjects:
+            self._subjects[name] = Subject()
+        return self._subjects[name]
+        
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
