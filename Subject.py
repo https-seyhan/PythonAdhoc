6a50f48afe4edb2788f26ad17d1e93cba8e49b26
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-25 10:15:26 +1100

    Add files via upload

---
+++ Subject.py
@@ -0,0 +1,18 @@
+import collections
+Grade = collections.namedtuple('Grade', ('score', 'weight'))
+
+class Subject(object):
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+        
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+        
+        
