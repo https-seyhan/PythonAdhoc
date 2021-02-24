7287657abcf5a24a96b445f0c6cd7327a427c63f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-25 10:15:51 +1100

    Add files via upload

---
+++ Gradebook.py
@@ -0,0 +1,19 @@
+from Student import *
+
+class Gradebook(object):
+    def __init__(self):
+        self._students = {}
+    
+    def student(self, name):
+        if name not in self._students:
+            self._students[name] = Student()
+        return self._students[name]
+        
+
+if __name__ == '__main__':
+    book = Gradebook()
+    albert = book.student('Albert Einstein')
+    math = albert.subject('Math')
+    math.report_grade(80, 0.10)
+    
+    print(albert.average_grade())
