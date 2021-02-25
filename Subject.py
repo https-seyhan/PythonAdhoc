e79bff6bfd75f791bdee7701d17c507fb8974486
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-26 09:01:38 +1100

    Add files via upload

--- Subject.py
+++ Subject.py
@@ -11,8 +11,11 @@ class Subject(object):
     def average_grade(self):
         total, total_weight = 0, 0
         for grade in self._grades:
+            print("Grade !!", grade)
             total += grade.score * grade.weight
             total_weight += grade.weight
-        return total / total_weight
+        print("Total !!!!", total)
+        print("Total Weight", total_weight)
+        return total 
         
         
