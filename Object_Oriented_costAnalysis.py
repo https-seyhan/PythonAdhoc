c7c2948652bb9d452a887541234e011e0378253d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-19 07:43:38 +1100

    Update Object_Oriented_costAnalysis.py

--- Object_Oriented_costAnalysis.py
+++ Object_Oriented_costAnalysis.py
@@ -13,7 +13,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetIte
 # Generate Spending class
 class Spending():
     def __init__(self):
-        print("TESST")
+        
         sb.set(style="whitegrid")
         pd.options.display.float_format = '{:.1f}'.format
         os.chdir('/home/saul/Desktop')
