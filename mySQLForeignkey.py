3230f0fb4e36bd87e5259ca140fb97325a20b10a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-21 22:48:07 +1000

    Add files via upload

--- mySQLForeignkey.py
+++ mySQLForeignkey.py
@@ -11,3 +11,44 @@ CREATE TABLE products(
     FOREIGN KEY (categoryId) 
         REFERENCES categories(categoryId)
 ) ENGINE=INNODB;
+
+create_ratings_table_query = """
+CREATE TABLE ratings (
+    movie_id INT,
+    reviewer_id INT,
+    rating DECIMAL(2,1),
+    FOREIGN KEY(movie_id) REFERENCES movies(id),
+    FOREIGN KEY(reviewer_id) REFERENCES reviewers(id),
+    PRIMARY KEY(movie_id, reviewer_id)
+)
+"""
+with connection.cursor() as cursor:
+    cursor.execute(create_ratings_table_query)
+    connection.commit()
+
+
+create_reviewers_table_query = """
+CREATE TABLE reviewers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+)
+"""
+with connection.cursor() as cursor:
+    cursor.execute(create_reviewers_table_query)
+    connection.commit()
+
+
+create_movies_table_query = """
+CREATE TABLE movies(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    release_year YEAR(4),
+    genre VARCHAR(100),
+    collection_in_mil INT
+)
+"""
+with connection.cursor() as cursor:
+    cursor.execute(create_movies_table_query)
+    connection.commit()
+
