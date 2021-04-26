e8a190d914a081595deea1521b1ee727a454d637
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-26 23:00:21 +1000

    Add files via upload

---
+++ pdfRead.py
@@ -0,0 +1,23 @@
+import PyPDF2
+import os
+os.chdir('/home/saul/Business')
+
+def Read(startPage, endPage):
+    global text
+    text = []
+    cleanText = ""
+    pdfFileObj = open('usaassangejudgement.pdf', 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    while startPage <= endPage:
+        pageObj = pdfReader.getPage(startPage)
+        text += pageObj.extractText()
+        startPage += 1
+    pdfFileObj.close()
+    for myWord in text:
+        if myWord != '\n':
+            cleanText += myWord
+    text = cleanText.split()
+    print(text)
+
+Read(0,0)
+
