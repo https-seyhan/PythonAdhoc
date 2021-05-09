8aeca6f9f564d3038a648db752542936e6103aa5
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-09 20:49:05 +1000

    Add files via upload

---
+++ python_sql_work.py
@@ -0,0 +1,18 @@
+cursor.execute ("""
+   UPDATE tblTableName
+   SET Year=%s, Month=%s, Day=%s, Hour=%s, Minute=%s
+   WHERE Server=%s
+""", (Year, Month, Day, Hour, Minute, ServerID))
+
+cursor.execute ("UPDATE tblTableName SET Year=%s, Month=%s, Day=%s, Hour=%s, Minute=%s WHERE Server='%s' " % (Year, Month, Day, Hour, Minute, ServerID))
+
+cursor.execute("UPDATE table_name SET field1=%s, ..., field10=%s WHERE id=%s", (var1,... var10, id))
+
+mycursor = mydb.cursor()
+  
+mycursor.execute("SELECT Concat(FirstName, LastName) AS fulldetail FROM Persons;")
+  
+myresult = mycursor.fetchall()
+  
+for x in myresult:
+  print(x)
