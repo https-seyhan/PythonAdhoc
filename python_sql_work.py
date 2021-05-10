557bd0a6e4ad4eda2762c682c7d18a96b02689ac
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-10 21:47:57 +1000

    Update python_sql_work.py

--- python_sql_work.py
+++ python_sql_work.py
@@ -5,7 +5,6 @@ cursor.execute ("""
 """, (Year, Month, Day, Hour, Minute, ServerID))
 
 cursor.execute ("UPDATE tblTableName SET Year=%s, Month=%s, Day=%s, Hour=%s, Minute=%s WHERE Server='%s' " % (Year, Month, Day, Hour, Minute, ServerID))
-
 cursor.execute("UPDATE table_name SET field1=%s, ..., field10=%s WHERE id=%s", (var1,... var10, id))
 
 mycursor = mydb.cursor()
