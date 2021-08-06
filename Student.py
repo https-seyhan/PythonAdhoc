0a3b090a6b95cc440c348e2dc81dbe261eb4ab22
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-06 15:51:30 +1000

    Update Student.py

--- Student.py
+++ Student.py
@@ -3,12 +3,12 @@ from Subject import *
 class Student(object):
     def __init__(self):
         self._subjects = {}
-   
+  
     def subject(self, name):
         if name not in self._subjects:
             self._subjects[name] = Subject()
         return self._subjects[name]
-    
+   
     def average_grade(self):
         print(self._subjects)
         total, count = 0, 0
