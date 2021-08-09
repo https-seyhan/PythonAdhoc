fbf769d87ce0d54447b42d8d7a1d4bc8ea106c22
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-10 08:00:11 +1000

    Update costAnalysis.py

--- costAnalysis.py
+++ costAnalysis.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from datetime import date
 from matplotlib import pyplot as plt
 
-
 def setEnvironment():
     #sb.set(style="whitegrid")
     plt.style.use('seaborn-whitegrid')
