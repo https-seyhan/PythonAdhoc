041e7c6ffa3ddbcec9ee8baad3ee9cbf15e50509
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-17 09:59:31 +1000

    Update pdfRead.py

--- pdfRead.py
+++ pdfRead.py
@@ -8,7 +8,7 @@ def Read(startPage, endPage):
     cleanText = ""
     pdfFileObj = open('usaassangejudgement.pdf', 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj) #PDF Reader
-    
+   
     while startPage <= endPage:
         pageObj = pdfReader.getPage(startPage)
         text += pageObj.extractText()
