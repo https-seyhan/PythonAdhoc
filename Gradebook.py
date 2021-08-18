c8a7bf106f64b383022b789fd19beebe6878697a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-19 00:52:50 +1000

    Update Gradebook.py

--- Gradebook.py
+++ Gradebook.py
@@ -8,7 +8,7 @@ class Gradebook(object):
         if name not in self._students:
             self._students[name] = Student()
         return self._students[name]
-       
+      
 if __name__ == '__main__':
     book = Gradebook()
     albert = book.student('Albert Einstein')
