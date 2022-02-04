2115f9d5df6de30383ebb331b6e807ef171bc1f1
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-02-05 10:44:34 +1100

    Update costAnalysis.py

--- costAnalysis.py
+++ costAnalysis.py
@@ -115,7 +115,7 @@ def plotBox(cost):
     print(coles.describe())
     markets = pd.DataFrame({'coles': coles, 'wolli': wolli})
     #markets.boxplot()
-   
+  
 setEnvironment()
 #cost = importData()
 shapeData(importData())
