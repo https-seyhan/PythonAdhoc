56e6f371080b782d44e27f2f4e4a71378de48089
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-12 11:11:33 +1000

    Update web_Data_NLP_Object_Oriented.py

--- web_Data_NLP_Object_Oriented.py
+++ web_Data_NLP_Object_Oriented.py
@@ -1,7 +1,7 @@
 import numpy as np
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
-nlp = spacy.load("en_core_web_sm") # SpaCy NLP corpus
+
 import urllib.request
 # Create Object that reads data of a web page and remove stop words and tokenize words.
 # The web page used is https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
