54d0b42b0005efdc1e6a4203cf43dd70d9b6215d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-12 11:43:44 +1000

    Update Student.py

--- Student.py
+++ Student.py
@@ -11,6 +11,7 @@ class Student(object):
     def average_grade(self):
         print(self._subjects)
         total, count = 0, 0
+        
         for subject in self._subjects.values():
             total += subject.average_grade()
             count += 1
