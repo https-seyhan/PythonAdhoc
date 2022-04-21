9fe90355462f4ac1c8e74800f3b991178a52f20a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-21 11:13:21 +1000

    Update Intelligent_Web_Crawling.py

--- Intelligent_Web_Crawling.py
+++ Intelligent_Web_Crawling.py
@@ -7,7 +7,7 @@ class AsxDataExtract(scrapy.Spider):
     start_urls = ["https://www.listcorp.com/asx/sectors/information-technology"] #start url
     #category = 'information-technology'
     #links = {}
-   
+  
     def parse(self, response):
         query = '//*[@class="v-datatable v-table theme--light"]//tbody/tr' #location to scrap data
         #query= '//*[@class ="v-table__overflow"]//tbody/tr'
