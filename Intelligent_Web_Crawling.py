aeba59a50e20e3cd30a0a369c07858232f3587d1
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-21 10:18:39 +1000

    Update Intelligent_Web_Crawling.py

--- Intelligent_Web_Crawling.py
+++ Intelligent_Web_Crawling.py
@@ -2,7 +2,7 @@ import scrapy
 #run as scrapy crawl asxdataextract -o output.csv in the terminal
 class AsxDataExtract(scrapy.Spider):
     name='asxdataextract'
-    allowed_domains = ["listcorp.com/asx/sectors",] # domains
+    allowed_domains = ["listcorp.com/asx/sectors",] # domain(s)
     start_urls = ["https://www.listcorp.com/asx/sectors/information-technology"]
     #category = 'information-technology'
     #links = {}
