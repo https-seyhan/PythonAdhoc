701fbdca3ba0e521b7ed825aafe467fe9bfd06cb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-23 13:05:49 +1000

    Update web_Data_NLP_Object_Oriented.py

--- web_Data_NLP_Object_Oriented.py
+++ web_Data_NLP_Object_Oriented.py
@@ -5,7 +5,6 @@ from spacy.lang.en.stop_words import STOP_WORDS
 nlp = spacy.load("en_core_web_sm") # SpaCy NLP corpus
 # Create Object that reads data of a web page and remove stop words and tokenize words.
 
-
 class webData():
 
     def __init__(self):
