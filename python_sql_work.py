f2cb6040ec62015d4c1dac5d0bfca0208c8c4169
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-11 14:04:59 +1000

    Update python_sql_work.py

--- python_sql_work.py
+++ python_sql_work.py
@@ -11,6 +11,6 @@ mycursor = mydb.cursor()
 mycursor.execute("SELECT Concat(FirstName, LastName) AS fulldetail FROM Persons;")
 
 myresult = mycursor.fetchall()
-
+#print results
 for x in myresult:
   print(x)
