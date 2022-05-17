7fd1d3f85ea6acd587d72fc4dd375b825fe5845f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-17 20:05:08 +1000

    Add files via upload

--- file_reads.py
+++ file_reads.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import os
+import psutil
+
 class readFiles:
 	name= 'read csv files'
 	print('Read Files')
 	
 	def __init__(self):
+		print(psutil.virtual_memory())
+		
 		
 	
-
 
 if '__name__' == '__main_':
 	files = readFiles()
