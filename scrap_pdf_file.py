cf26958b5932d2d5aacd36ebbc4088c07b6acac0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-17 20:34:55 +1000

    Update scrap_pdf_file.py

--- scrap_pdf_file.py
+++ scrap_pdf_file.py
@@ -14,7 +14,7 @@ def Read(startPage, endPage):
         pageObj = pdfReader.getPage(startPage)
         text += pageObj.extractText()
         startPage += 1
-        
+       
     pdfFileObj.close() #
     for myWord in text:
         if myWord != '\n':
