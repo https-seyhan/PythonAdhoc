0188a09cdc0cf38f736ed7f8f073ca6beed6e175
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-17 21:13:13 +1000

    Add files via upload

--- file_reads.py
+++ file_reads.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import psutil
+import datetime
+import json
+import eml_parser
 
 class readFiles:
 	name= 'read csv files'
@@ -11,15 +14,19 @@ class readFiles:
 		#self.read_memory()
 		#print(psutil.virtual_memory())
 		print('Virtual Memory Percent ', psutil.virtual_memory().percent)
+		print(psutil)
+		self.read_eml_file()
 		
 	def read_memory(self):
 		print(psutil.virtual_memory())
-		
-		
+	
+	def read_eml_file(self):
+		with open('sample_file.eml', 'rb') as email:
+  			raw_email = email.read()
+
 	
 
-
-files = readFiles()
-	#readFiles()
-files
+#files = readFiles()
+readFiles()
+#files
 
