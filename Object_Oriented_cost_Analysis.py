d9f6547ea662a22647f5491a0e18dad3b3676e40
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-06-21 16:47:41 +1000

    Update Object_Oriented_cost_Analysis.py

--- Object_Oriented_cost_Analysis.py
+++ Object_Oriented_cost_Analysis.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 
 # Generate Spending class
 class Spending():
-    def __init__(self):  
+    def __init__(self): 
         sb.set(style="whitegrid")
         pd.options.display.float_format = '{:.1f}'.format
         os.chdir('/home/saul/Desktop')
