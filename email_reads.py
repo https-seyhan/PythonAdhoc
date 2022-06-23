d0dbf6359cb4d29232bafa57d142e9a226151cdf
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-06-23 21:27:39 +1000

    Update email_reads.py

--- email_reads.py
+++ email_reads.py
@@ -39,7 +39,6 @@ class readFiles:
 		#print('Received Foremail', json_object['header']['received_foremail'], '\n')
 		#print('Received IP', json_object['header']['received_ip'], '\n')
 
-
 		for item in json_object['header']:
 			#print(item)
 			#print(item['date'])
