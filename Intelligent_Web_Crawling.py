ceb0ee7ca47f9779644de3f0f7e3d57516f64537
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-06-23 21:05:21 +1000

    Update Intelligent_Web_Crawling.py

--- Intelligent_Web_Crawling.py
+++ Intelligent_Web_Crawling.py
@@ -1,7 +1,7 @@
 import scrapy
 
 #run as scrapy crawl asxdataextract -o output.csv in the terminal
-#class
+#class crawl
 class AsxDataExtract(scrapy.Spider):
     
     name='asxdataextract' #name of the crawl
